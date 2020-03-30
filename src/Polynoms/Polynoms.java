1e74749265960f89750f15cd52861ac0a330ee2d
milerova-marketa <marketa.milerova@tul.cz>
2020-03-31 01:48:39 +0200

    Create Polynoms.java

---
+++ src/Polynoms/Polynoms.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polynoms;
+
+/**
+ *
+ * @author Marketa.Milerova
+ */
+public class Polynoms {
+
+    //díky tomuhle se z knihovní třídy nestane objekt
+    private Polynoms() {
+
+    }
+
+    public static void main(String[] args) {
+        Polynom p1 = Polynom.getInstance(1,2,3,4,5);
+        Polynom p2 = Polynom.getInstance(5, 3, 2);
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(sum(p1, p2));
+        System.out.println(multiply(p1, p2));
+    }
+
+    public static Polynom sum(Polynom a, Polynom b) {
+        Polynom max = (a.getDegree() > b.getDegree()) ? a : b;
+        Polynom min = (a.getDegree() > b.getDegree()) ? b : a;
+        double[] sumCoef = new double[max.getDegree() + 1];
+        for (int i = 0; i < max.getDegree() + 1; i++) {
+            sumCoef[i] = max.getCoefAt(i);
+        }
+        for (int i = 0; i < min.getDegree() + 1; i++) {
+            sumCoef[i] += min.getCoefAt(i);
+        }
+        return Polynom.getInstanceReverted(sumCoef);
+    }
+
+    public static Polynom multiply(Polynom a, Polynom b) {
+        double[] multiCoef = new double[a.getDegree() + b.getDegree() + 1];
+        for (int i = 0; i < a.getDegree() + 1; i++) {
+            for (int j = 0; j < b.getDegree() + 1; j++) {
+                multiCoef[i + j] += (a.getCoefAt(i) * b.getCoefAt(j));
+            }
+        }
+        return Polynom.getInstanceReverted(multiCoef);
+    }
+}
