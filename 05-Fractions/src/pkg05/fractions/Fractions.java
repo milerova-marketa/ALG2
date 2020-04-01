c6eebe7b60ee92c5ff57a104fce14b0713a854e0
milerova-marketa <marketa.milerova@tul.cz>
2020-04-01 14:27:57 +0200

    Update Fractions.java

--- 05-Fractions/src/pkg05/fractions/Fractions.java
+++ 05-Fractions/src/pkg05/fractions/Fractions.java
@@ -12,11 +12,8 @@ package pkg05.fractions;
 public class Fractions {
 
     public static void main(String[] args) {
-        Fraction z1 = new Fraction();
-        z1.setCitatel(2);
-        z1.setJmenovatel(5);
+        Fraction z1 = new Fraction(8, 20);
         Fraction z2 = new Fraction(3, 4);
-        z2.setJmenovatel(8);
         System.out.println(FractionsCalculator.add(z1, z2));
         System.out.println(FractionsCalculator.substract(z1, z2));
         System.out.println(FractionsCalculator.substract(z2, z1));
